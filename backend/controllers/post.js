c220605ce7856a6b2f3f311d5cb6d081316ec649
Jairo Alzate <jairo.alzate@siesa.com>
2021-08-18 10:03:35 -0500

    Finish api post with middleware

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -1,7 +1,24 @@
 const Post = require("../models/post");
 
-const registerPost = async (req, res) => {};
+const registerPost = async (req, res) => {
+  if (!req.body.text || !req.body.hashtag)
+    return res.status(400).send("Incomplete data");
 
-const listPost = async (req, res) => {};
+  const post = new Post({
+    userId: req.user._id,
+    text: req.body.text,
+    hashtag: req.body.hashtag,
+  });
+
+  const result = await post.save();
+  if (!result) return res.status(400).send("Failed to register post");
+  return res.status(200).send({ result });
+};
+
+const listPost = async (req, res) => {
+  const post = await Post.find().populate("userId").exec();
+  if (!post || post.length === 0) return res.status(400).send("No posts");
+  return res.status(200).send({ post });
+};
 
 module.exports = { registerPost, listPost };
