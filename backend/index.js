39e13ea85e24f1bdc68662f8fc45fea6e9349164
Jairo Alzate <jairo.alzate@siesa.com>
2021-08-18 09:37:05 -0500

    Add login to project

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const { dbConnection } = require("./db/db");
 const User = require("./routers/user");
 const Post = require("./routers/post");
+const Auth = require("./routers/auth");
 require("dotenv").config();
 
 const app = express();
@@ -10,6 +11,7 @@ const app = express();
 app.use(express.json());
 app.use("/api/user", User);
 app.use("/api/post", Post);
+app.use("/api/auth", Auth);
 
 app.listen(process.env.PORT, () =>
   console.log("Backend server running in port: ", process.env.PORT)
