e6538728c40463ab3bb73ec13c7b27dfd8d5c393
Jairo Alzate <jairo.alzate@siesa.com>
2021-08-18 09:16:04 -0500

    Finish api to user

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,7 +1,38 @@
 const User = require("../models/user");
+const bcrypt = require("bcrypt");
 
-const registerUser = async (req, res) => {};
+const registerUser = async (req, res) => {
+  if (!req.body.name || !req.body.email || !req.body.password)
+    return res.status(400).send("Incomplete data");
 
-const listUser = async (req, res) => {};
+  let existing = await User.findOne({ email: req.body.email });
+  if (existing)
+    return res
+      .status(400)
+      .send("Transaction rejects: the email already exists");
+
+  let hash = await bcrypt.hash(req.body.password, 10);
+
+  let user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: hash,
+  });
+
+  let result = await user.save();
+  if (!result) return res.status(400).send("Error to register user");
+  try {
+    let jwt = user.generateJWT();
+    return res.status(200).send({ jwt });
+  } catch (e) {
+    return res.status(400).send("Error to register user");
+  }
+};
+
+const listUser = async (req, res) => {
+    let user = await User.find({ name: RegExp(req.params["name"], "i") });
+    if(!user || user.length === 0) return res.status(400).send("No users");
+    return res.status(200).send({ user });
+};
 
 module.exports = { registerUser, listUser };
